86933f4913ef23aa0b41868bce560987cbbfd6f2
vibincholayil <vibinnath.c@gmail.com>
2025-02-02 22:27:15 +0000

    update

--- README.md
+++ README.md
@@ -5,5 +5,7 @@
 `sudo apt update -y`  
 installed Terraform suggested by Hashicorp: https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli  
 Then, i have created a terraform configuration file: https://registry.terraform.io/providers/hashicorp/aws/latest/docs  
+`terraform init` Terraform has been successfully initialized!   
+Configure AWS CLI: `sudo apt insyall awscli -y` `aws configure`  
+`terraform plan`  
  
-
--- main.tf
+++ main.tf
@@ -9,12 +9,53 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region  = "us-west-2"
 }
 
+# Reference an existing VPC
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = "vpc-0e4118d674e707618"  # Your existing VPC ID
+  cidr_block              = "12.0.0.0/16"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+# Create a security group inside the existing VPC
+resource "aws_security_group" "microservices_SG"  {
+  vpc_id = "vpc-0e4118d674e707618"  # Your existing VPC ID
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "microservices_SG"
+  }
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-091f18e98bc129c4e"
   instance_type = "t2.micro"
 
   tags = {
